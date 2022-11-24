62600d096b6f814d3025aa1cd889219dea3e7622
Lawrence Mensah Ofoli <lawrenceofoli@gmail.com>
2022-11-24 22:47:59 +0000

    added ui for chat input form

--- src/features/chat-dashboard/components/form/ChatInput.tsx
+++ src/features/chat-dashboard/components/form/ChatInput.tsx
@@ -1,5 +1,27 @@
+import { Grid } from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
+import AttachFileIcon from "@mui/icons-material/AttachFile";
 import classes from "../../styles/chat-input.module.css";
 
 export default function ChatInput() {
-  return <div className={classes.main}>Chat input</div>;
+  return (
+    <div className={classes.main}>
+      <div className={classes.grid_container}>
+        <div className={classes.icon_container}>
+          <AttachFileIcon className={classes.icon} />
+        </div>
+        <div className={classes.input_container}>
+          <div
+            className={classes.input}
+            contentEditable
+            role="textbox"
+            title="Write a message"
+          ></div>
+        </div>
+        <div className={classes.icon_container}>
+          <SendIcon className={classes.icon} />
+        </div>
+      </div>
+    </div>
+  );
 }
