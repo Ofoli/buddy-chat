97d1f754cfcbc249874f9fa740c7680c3b4b411c
Lawrence Mensah Ofoli <lawrenceofoli@gmail.com>
2023-06-01 00:16:18 +0000

    restructured login-register feature folder

---
+++ src/features/login-register/containers/login-register-base.tsx
@@ -0,0 +1,48 @@
+import classes from "../styles/login-register.module.css";
+import { Paper, Grid } from "@mui/material";
+import { BaseButton, AppLogo } from "../index/imports";
+
+const paperStyles = {
+  width: "70%",
+  height: "calc(100% - 200px)",
+};
+
+export default function LoginRegisterBase() {
+  return (
+    <Paper elevation={0} style={paperStyles}>
+      <Grid
+        container
+        alignItems="center"
+        justifyContent="center"
+        className={classes.login__container}
+      >
+        <Grid item md={5} className={classes.side_info__container}>
+          <AppLogo />
+          <Grid
+            className={classes.side_info__body}
+            container
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+            gap={2}
+          >
+            <h1>Hello, Friend</h1>
+            <p className={classes.side_info__message}>
+              Enter your personal details and let us shoot with other Friends.
+              Stay connect at all times
+            </p>
+            <div className={classes.side_info__btn_container}>
+              <BaseButton
+                label="Register"
+                onClick={() => console.log("click")}
+              />
+            </div>
+          </Grid>
+        </Grid>
+        <Grid item md={7} className={classes.login__form_container}>
+          form
+        </Grid>
+      </Grid>
+    </Paper>
+  );
+}
